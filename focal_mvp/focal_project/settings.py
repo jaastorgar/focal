a346c7d062d6ef6b6f9d6ba8d45116e80ab1caf1
Javier <ja.astorgar@duocuc.cl>
2025-10-22 20:41:09 -0300

    se agregar restablecimiento de contraseña.

--- focal_mvp/focal_project/settings.py
+++ focal_mvp/focal_project/settings.py
@@ -124,6 +124,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Configuración para Gmail
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = config("EMAIL_HOST", default="")
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="FOCAL <plataforma.focal@gmail.com>")
+EMAIL_TIMEOUT = config("EMAIL_TIMEOUT", cast=int, default=20)
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- focal_mvp/focal_project/urls.py
+++ focal_mvp/focal_project/urls.py
@@ -17,10 +17,45 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('landing.urls', 'landing'), namespace='landing')),
     path('', include('inventario.urls')),
     path('finanza/', include('finanza.urls')),
+    
+    # Password reset
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset_form.html",
+            email_template_name="accounts/password_reset_email.html",
+            subject_template_name="accounts/password_reset_subject.txt",
+            success_url=reverse_lazy("password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url=reverse_lazy("password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
